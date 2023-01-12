a2492a0bb6d67e88bbe60d651be94db1d8bd6408
LN6666 <2272153619@qq.com>
2023-01-13 01:21:39 +0800

    commit

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ljn/ChatServer/src/server/chatserver.cpp" "src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o.d"
+  "/home/ljn/ChatServer/src/server/chatservice.cpp" "src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o.d"
+  "/home/ljn/ChatServer/src/server/database/database.cpp" "src/server/CMakeFiles/ChatServer.dir/database/database.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/database/database.cpp.o.d"
+  "/home/ljn/ChatServer/src/server/main.cpp" "src/server/CMakeFiles/ChatServer.dir/main.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/main.cpp.o.d"
+  "/home/ljn/ChatServer/src/server/model/FriendModel.cpp" "src/server/CMakeFiles/ChatServer.dir/model/FriendModel.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/model/FriendModel.cpp.o.d"
+  "/home/ljn/ChatServer/src/server/model/UserModel.cpp" "src/server/CMakeFiles/ChatServer.dir/model/UserModel.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/model/UserModel.cpp.o.d"
+  "/home/ljn/ChatServer/src/server/model/groupModel.cpp" "src/server/CMakeFiles/ChatServer.dir/model/groupModel.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/model/groupModel.cpp.o.d"
+  "/home/ljn/ChatServer/src/server/model/offlinemessagemodel.cpp" "src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o.d"
+  "/home/ljn/ChatServer/src/server/redis/redis.cpp" "src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/server/CMakeFiles/ChatServer.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "../../../bin/ChatServer"
+  "../../../bin/ChatServer.pdb"
+  "CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "CMakeFiles/ChatServer.dir/chatserver.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "CMakeFiles/ChatServer.dir/chatservice.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/database/database.cpp.o"
+  "CMakeFiles/ChatServer.dir/database/database.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/main.cpp.o"
+  "CMakeFiles/ChatServer.dir/main.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/model/FriendModel.cpp.o"
+  "CMakeFiles/ChatServer.dir/model/FriendModel.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/model/UserModel.cpp.o"
+  "CMakeFiles/ChatServer.dir/model/UserModel.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/model/groupModel.cpp.o"
+  "CMakeFiles/ChatServer.dir/model/groupModel.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  "CMakeFiles/ChatServer.dir/redis/redis.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
